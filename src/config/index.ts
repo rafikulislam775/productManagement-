184a010f2d98b6701dcc17c4eee5f7c63612a4f0
rafikulislam775 <islamrafikul.jp@gmail.com>
2024-05-23 08:02:50 +0900

    create a server with mongoDB and also setup env config

---
+++ src/config/index.ts
@@ -0,0 +1,7 @@
+// eve code use korar jnn
+import dotenv from "dotenv";
+dotenv.config();
+export default {
+  port: process.env.PORT,
+  db_url: process.env.DB_URL,
+};
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,17 @@
 import mongoose from "mongoose";
 import app from "./app";
+import config from "./config";
+
+// dotenv.config();
 
 async function main() {
   try {
     // connect to MongoDB atlast path
-    await mongoose.connect(process.env.DB_URL);
+
+    await mongoose.connect(config.db_url as string); // as string ami sure eta string asbe
 
     app.listen(process.env.PORT, () => {
-      console.log(`Example app listening on port ${process.env.PORT}`);
+      console.log(`Example app listening on port ${config.port}`);
     });
   } catch (err) {
     console.log(err);

5150e6b8939fb3287a68489ae2b501fc9f8b6ae0
rafikulislam775 <islamrafikul.jp@gmail.com>
2024-05-23 11:11:33 +0900

    create products Schema

---
+++ src/modules/products/products.models.ts
@@ -0,0 +1,58 @@
+import { Schema } from "mongoose";
+import { TInventory, TProducts, TVariant } from "./products.interface";
+//! <TProducts> Schema corresponding to the coming interface. like check
+
+//2. Create a Schema corresponding to the document interface.
+//sub schema
+const variantSchema = new Schema<TVariant>({
+  type: {
+    type: String,
+    required: true,
+  },
+  value: {
+    type: String,
+    required: true,
+  },
+});
+// sub schema
+const inventorySchema = new Schema<TInventory>({
+  quantity: {
+    type: Number,
+    required: true,
+  },
+  inStock: {
+    type: Boolean,
+    required: true,
+  },
+});
+//main schema
+const productSchema = new Schema<TProducts>({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  tags: {
+    type: [String],
+    required: true,
+  },
+  variants: {
+    type: [variantSchema],
+    required: true,
+  },
+  inventory: {
+    type: inventorySchema,
+    required: true,
+  },
+});
